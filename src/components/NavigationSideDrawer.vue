b40dba6dffcc48f857c97ce8d5c600e619ae0b4f
Soufiane Benzaoui <f>
2021-01-20 20:35:07 +0100

    Update navigation drawer

---
+++ src/components/NavigationSideDrawer.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-navigation-drawer
+    app
+    :value="value"
+    clipped
+    :mini-variant="miniVariant"
+    color="amber lighten-1"
+  >
+    <v-list dense class="amber" shaped>
+      <template v-for="(item, i) in items">
+        <v-row v-if="item.heading" :key="i" align="center">
+          <v-col cols="6">
+            <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
+          </v-col>
+          <!-- <v-col cols="6" class="text-right">
+              <v-btn small text>edit</v-btn>
+            </v-col> -->
+        </v-row>
+        <v-divider v-else-if="item.divider" :key="i" dark class="my-4" />
+
+        <v-list-item
+          v-else
+          :key="i"
+          link
+          :to="item.to"
+          :href="item.href"
+          :disabled="!item.activated"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="black--text">{{
+              item.text
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: Boolean, required: false },
+    miniVariant: Boolean,
+  },
+
+  data() {
+    return {
+      items: [
+        { icon: "mdi-home-roof", text: "Home", to: "/", activated: true },
+        {
+          icon: "lightbulb_outline",
+          text: "Projects",
+          to: "/projects/",
+          activated: true,
+        },
+        {
+          icon: "mdi-blogger",
+          text: "Blog",
+          href: "https://soubenz.medium.com/",
+          activated: true,
+        },
+        // { divider: true },
+        // { heading: "Labels" },
+        {
+          icon: "mdi-code-tags",
+          text: "Code Snippets",
+          to: "/snippets/",
+          activated: false,
+        },
+        {
+          icon: "domain",
+          text: "Career Path",
+          to: "/career/",
+          activated: true,
+        },
+        // { icon: "chat_bubble", text: "Contact", to: "" },
+        {
+          icon: "mdi-linkedin",
+          text: "My Linkedin",
+          href: "https://www.linkedin.com/in/soufiane-benzaoui/",
+          activated: true,
+        },
+        {
+          icon: "mdi-xml",
+          text: "Github",
+          href: "https://github.com/soubenz",
+          activated: true,
+        },
+        { icon: "email", text: "Email me", to: "/contact/", activated: true },
+      ],
+    };
+  },
+  mounted() {},
+
+  computed: {},
+};
+</script>
+
+<style></style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,64 +2,79 @@
   <v-app id="keep">
     <v-app-bar app clipped-left color="amber">
       <v-app-bar-nav-icon @click="drawer = !drawer" />
+
       <span class="title ml-3 mr-5">
         Benzaoui&nbsp;
         <span class="font-weight-light">Soufiane</span>
       </span>
+      <v-spacer />
+      <span class="headline font-weight-bold">{{ pageTitle }}</span>
+
       <!-- <v-text-field solo-inverted flat hide-details label="Search" prepend-inner-icon="search" /> -->
 
       <v-spacer />
     </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" app clipped color="amber lighten-1">
-      <v-list dense class="amber">
-        <template v-for="(item, i) in items">
-          <v-row v-if="item.heading" :key="i" align="center">
-            <v-col cols="6">
-              <v-subheader v-if="item.heading">{{ item.heading }}</v-subheader>
-            </v-col>
-            <v-col cols="6" class="text-right">
-              <v-btn small text>edit</v-btn>
-            </v-col>
-          </v-row>
-          <v-divider v-else-if="item.divider" :key="i" dark class="my-4" />
-          <v-list-item v-else :key="i" link>
-            <v-list-item-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title class="black--text">{{ item.text }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-content>
-      <v-container fluid class="grey lighten-4">
+    <navigation-side-drawer
+      v-model="drawer"
+      :mini-variant="$vuetify.breakpoint.mobile"
+    />
+    <!-- <navigation-side-drawer v-model="drawer" :mini-variant="true" /> -->
+    <v-main>
+      <v-container>
         <slot />
       </v-container>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import NavigationSideDrawer from "@/components/NavigationSideDrawer";
+
 export default {
+  components: { NavigationSideDrawer },
   props: {
-    source: String
+    pageTitle: String,
   },
   data: () => ({
-    drawer: null,
+    drawer: false,
+    test: false,
     items: [
-      { icon: "lightbulb_outline", text: "Projects", to: "" },
-      { icon: "mdi-blogger", text: "Blog", to: "" },
+      { icon: "mdi-home-roof", text: "Home", to: "/", activated: true },
+      {
+        icon: "lightbulb_outline",
+        text: "Projects",
+        to: "/projects/",
+        activated: true,
+      },
+      {
+        icon: "mdi-blogger",
+        text: "Blog",
+        href: "https://soubenz.medium.com/",
+        activated: true,
+      },
       // { divider: true },
       // { heading: "Labels" },
-      { icon: "mdi-code-tags", text: "Code Snippets", to: "" },
+      {
+        icon: "mdi-code-tags",
+        text: "Code Snippets",
+        to: "/snippets/",
+        activated: false,
+      },
+      { icon: "domain", text: "Career Path", to: "/career/", activated: true },
       // { icon: "chat_bubble", text: "Contact", to: "" },
-      { icon: "mdi-linkedin", text: "My Linkedin", to: "" },
-      { icon: "mdi-microsoft-github", text: "Github", to: "" },
-      { icon: "email", text: "Email me", to: "" }
+      {
+        icon: "mdi-linkedin",
+        text: "My Linkedin",
+        href: "https://www.linkedin.com/in/soufiane-benzaoui/",
+        activated: true,
+      },
+      {
+        icon: "mdi-xml",
+        text: "Github",
+        href: "https://github.com/soubenz",
+        activated: true,
+      },
+      { icon: "email", text: "Email me", to: "/contact/", activated: true },
 
       // { divider: true },
       // { icon: "archive", text: "Archive" },
@@ -70,8 +85,8 @@ export default {
       // { icon: "help", text: "Help" },
       // { icon: "phonelink", text: "App downloads" },
       // { icon: "keyboard", text: "Keyboard shortcuts" }
-    ]
-  })
+    ],
+  }),
 };
 </script>
 
